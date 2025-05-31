ddc581d721480a5ca4e57499e022e277fff12626
creative007official <creativesiteyt@gmail.com>
2025-05-31 19:45:21 +0545

    Second Commit

--- app/api/route.js
+++ app/api/route.js
@@ -1,12 +1,11 @@
 import { serve } from "inngest/next";
-import { inngest } from "@/config/inngest";
+import { inngest, syncUserData, updateUserData, deleteUserData } from "@/config/inngest";
 
-// Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
-    syncUserCreation,
-    syncUserUpdation,
-    syncUserDeletion,
+    syncUserData,
+    updateUserData,
+    deleteUserData,
   ],
 });
