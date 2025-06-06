10930e9fa043c9bb650d08c256af585c496cb94f
Aaditya Karki <152679463+aadityakarki007@users.noreply.github.com>
2025-06-06 13:57:13 +0545

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,28 @@ const outfit = Outfit({ subsets: ['latin'], weight: ["300", "400", "500"] });
 export const metadata = {
   title: "EShop - Your choice to buy",
   description: "EShop - Your choice to buy. The best online shopping experience.",
+  openGraph: {
+    title: "EShop - Your choice to buy",
+    description: "EShop - Your choice to buy. The best online shopping experience.",
+    url: "https://hamroeshop.com",
+    siteName: "EShop",
+    images: [
+      {
+        url: "https://hamroeshop.com/assets/og.png", // Make sure this image exists and is publicly accessible
+        width: 1200,
+        height: 630,
+        alt: "EShop Preview Image",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "EShop - Your choice to buy",
+    description: "EShop - Your choice to buy. The best online shopping experience.",
+    images: ["https://hamroeshop.com/assets/og.png"],
+  },
 };
 
 export default function RootLayout({ children }) {
