53db441393678c5e5b4b32ce00986c88e80fc12f
creative007official <creativesiteyt@gmail.com>
2025-06-14 10:29:38 +0545

    add

--- components/HomeProducts.jsx
+++ components/HomeProducts.jsx
@@ -9,10 +9,8 @@ const HomeProducts = () => {
   return (
     <div className="flex flex-col items-center pt-14">
       <p className="text-2xl font-medium text-left w-full">Popular products</p>
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 mt-6 pb-14 w-full">
-        {products.filter(product => product.isPopular).map((product, index) => (
-          <ProductCard key={index} product={product} />
-        ))}
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 flex-col items-center gap-6 mt-6 pb-14 w-full">
+        {products.filter(product => product.isPopular).map((product, index) => <ProductCard key={index} product={product} />)}
       </div>
       <button onClick={() => { router.push('/all-products') }} className="px-12 py-2.5 border rounded text-gray-500/70 hover:bg-slate-50/90 transition">
         See more
